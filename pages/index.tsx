29f93fb683be68bc2a767ce150c1030429d927a1
9cats <123337671@qq.com>
2023-03-31 13:27:29 +0800

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ type Status = {
 };
 
 export default function Index() {
+  const [connected, setConnected] = useState(false);
   const [status, setStatus] = useState<Status>({
     initialized: false,
     name: "",
@@ -27,10 +28,14 @@ export default function Index() {
     if (!isBrowser) return;
     const timer = setInterval(() => {
       try {
-        axios.get("http://127.0.0.1:9887/getUser").then((res) => {
-          res.data.name = res.data.name.split("Q群")[0]
-          setStatus(res.data);
-        }).catch(e => {})
+        axios
+          .get("http://127.0.0.1:9887/getUser")
+          .then((res) => {
+            res.data.name = res.data.name.split("Q群")[0];
+            setStatus(res.data);
+            setConnected(true);
+          })
+          .catch((e) => {});
       } catch {}
     }, 1000);
 
@@ -40,11 +45,23 @@ export default function Index() {
     };
   }, []);
 
-  return <div className="app">{status.initialized ? <div>
-    <div>服务器信息：</div>
-    <div>名称：{status.name}</div>
-    <div>地图：{status.map}</div>
-    {status.mapchiname? <div>译名：{status.mapchiname}</div> : ""}
-    <div>地址：{`${status.host}:${status.port}`}</div>
-  </div> : <div>未连接到小帮手...</div>}</div>;
+  return (
+    <div className="app">
+      {connected ? (
+        status.initialized ? (
+          <div>
+            <div>服务器信息：</div>
+            <div>名称：{status.name}</div>
+            <div>地图：{status.map}</div>
+            {status.mapchiname ? <div>译名：{status.mapchiname}</div> : ""}
+            <div>地址：{`${status.host}:${status.port}`}</div>
+          </div>
+        ) : (
+          <div>已连接到小帮手.</div>
+        )
+      ) : (
+        <div>未连接到小帮手...</div>
+      )}
+    </div>
+  );
 }
